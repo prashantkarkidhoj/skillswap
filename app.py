87a624fa20f1cc22661fcf101104cc4d30c4d5ba
Prashant Karki <shadowdigitalco2@gmail.com>
2026-08-19 16:29:23 -0400

    add basic flask app with home route

--- app.py
+++ app.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def home():
     return "SkillSwap is running!"
 
-def add_numbers(a, b):
-    return a + b
-
-print(add_numbers(3, 5))
-
 if __name__ == "__main__":
     app.run(debug=True)
